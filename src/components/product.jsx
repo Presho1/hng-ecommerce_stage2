c13ed62f1bcd70bb7d83f3ced39013459ea376d9
precious <preciousolayinka8@gmail.com>
2024-07-13 20:54:56 +0100

    tried to fetch product from timbu

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,24 +1,44 @@
 import { IoMdHeartEmpty } from "react-icons/io"; 
-import soap from '../assets/images/cera.png'; 
-
+import { useEffect, useState } from "react";
+// import { soap } from '../assets/images/cera.png'
 
 const Product = () => {
+    const [ products, setProducts ] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://api.timbu.cloud/products?organization_id=b2c3981fbb5b472884e05363b368ad66&APP_ID=6RH94XVS3N23MWL&API_KEY=c122f80bd91e497e8dae9854632158f220240713183306648782');
+                const jsonResult = await response.json();
+                setProducts(jsonResult);
+            } catch (error) {
+                console.error("Error fetching products:", error);
+            }
+        }
+
+        fetchData();
+    }, []);
+
     return (
         <>
-        <div className="box">
-            <IoMdHeartEmpty size={20} style={{position: 'absolute', right: '70%'}}/>
-            <img src={soap} alt="" className='boxImg' />
-            <div className="product-info">
-                <h3 className='boxTitle'>Cerave Foaming Cleanser 16 FL OZ/473ml</h3>
-                <p className='boxPrice'> #14,499</p>
-                <div className="product-button">
-                    <button style={{marginRight: 14}} className="button-increment"> - 1 + </button>
-                    <button className='button-cart'><span>ADD TO CART</span></button>
-                </div>
+            <div className="product-list">
+                {products.map((product) => (
+                    <div key={product.id} className="box">
+                        <IoMdHeartEmpty size={20} style={{position: 'absolute', right: '70%'}} />
+                        <img src={product.image} alt={product.name} className='boxImg' />
+                        <div className="product-info">
+                            <h3 className='boxTitle'>{product.name}</h3>
+                            <p className='boxPrice'>{product.price}</p>
+                            <div className="product-button">
+                                <button style={{marginRight: 14}} className="button-increment"> - 1 + </button>
+                                <button className='button-cart'><span>ADD TO CART</span></button>
+                            </div>
+                        </div>
+                    </div>
+                ))}
             </div>
-        </div>
-    </>
-    )
+        </>
+    );
 }
 
-export default Product
+export default Product;
